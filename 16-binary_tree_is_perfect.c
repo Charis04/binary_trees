d37fc771aa05baaeee6a801953470043ed5f40be
Charis <charisadu@gmail.com>
2024-04-12 01:04:11 +0000

    task 16: Write a function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,3 +37,21 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return ((binary_tree_is_full(tree) && left_height == right_height));
 }
 
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
+ * Return: 1 if full and 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
